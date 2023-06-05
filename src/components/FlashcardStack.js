376f239fc726b694f43ceac14d4ccef0d999c206
gbarn360 <gbarn360@gmail.com>
2023-06-05 11:31:58 -0400

    worked on flashcard page

---
+++ src/components/FlashcardStack.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react"
+
+export default function FlashcardStack({ term, definition, onChange }) {
+
+    const [flipped, setFlip] = useState(false);
+    const [changeCardStyle, setChangeCardStyle] = useState("");
+    useEffect(() => {
+
+        setTimeout(() => { setChangeCardStyle("") }, 200);
+        setChangeCardStyle("transition  -translate-y-16");
+    }, [onChange])
+
+
+
+
+    return (
+        <div>
+            <div onClick={() => { flipped == false ? setFlip(true) : setFlip(false) }} className={flipped == true ? `absolute bg-white shadow-lg rounded-lg w-1/2 flex justify-center items-center p-10 top-1/3 left-1/4 h-72 ${changeCardStyle} z-20` : `absolute bg-white shadow-lg rounded-lg w-1/2 flex justify-center items-center p-10 top-1/3 left-1/4 h-72 ${changeCardStyle} z-20`}>
+                <h1 className={flipped == false ? "block text-center text-2xl" : "hidden text-center text-2xl"}>{term}</h1>
+                <h1 className={flipped == false ? "hidden text-center text-2xl" : "block text-center text-2xl"}>{definition}</h1>
+            </div>
+            <div className="absolute bg-white shadow-lg rounded-lg w-1/2 flex justify-center items-center p-10 top-1/3 left-1/4 h-72 z-10">
+
+            </div>
+        </div>
+
+    )
+}
--- src/screens/FlashcardSetPage.js
+++ src/screens/FlashcardSetPage.js
@@ -1,30 +1,50 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "../components/NavBar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import NavBar from "../components/NavBar";
+import FlashcardStack from "../components/FlashcardStack";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 
 export default function FlashcardSetPage() {
-
-    const { subjectName } = useParams();
-    const { flashcardSet } = useParams();
-    const { id } = useParams();
-    const [flashcards, setFlashcardSets] = useState([]);
+    const { subjectName, flashcardSet, id } = useParams();
+    const [flashcards, setFlashcards] = useState([]);
+    const [currentFlashcard, setCurrentFlashcard] = useState();
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
         axios.post("http://localhost:8000/getFlashcards.php", {
             item: {
                 setId: id,
-            }
-        }).then(response => console.log(response.data))
-    }, [])
+            },
+        }).then(response => {
+            console.log(response.data);
+            setFlashcards(response.data);
+            setCurrentFlashcard(response.data[0]);
+        });
+    }, []);
 
     return (
-        <div className="w-screen h-auto bg-slate-100 flex flex-col ">
+        <div className="w-screen h-screen bg-slate-100 flex flex-col">
             <NavBar />
-            <div className="flex flex-col ">
-
+            <h1 className="text-center text-5xl text-slate-800 font-bold mt-5">
+                {flashcardSet}
+            </h1>
+            <div className="flex flex-col items-center">
+                <div className="flex flex-row justify-between w-1/2 mt-6">
+                    <AiOutlineArrowLeft className="text-2xl" onClick={() => { if (count != 0) { setCount(count - 1); setCurrentFlashcard(flashcards[count - 1]) } else setCount(count) }} />
+                    <h2 className="text-center text-xl  font-medium">{count + 1} / {flashcards.length}</h2>
+                    <AiOutlineArrowRight className="text-2xl" onClick={() => { if (count != flashcards.length - 1) { setCount(count + 1); setCurrentFlashcard(flashcards[count + 1]) } else setCount(count) }} />
+                </div>
+                {currentFlashcard && (
+                    <FlashcardStack
+                        term={currentFlashcard.term}
+                        definition={currentFlashcard.def}
+                        onChange={count}
+                    />
+                )}
             </div>
-        </div >
-    )
+        </div>
+    );
 }
